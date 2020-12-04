2d0002732eb78984f0aca154f4eb741d54f35970
Vid Potocnik <vidpotocnik.osebni@gmail.com>
2020-12-04 11:13:45 +0100

    Day 4 task 1

---
+++ dayfour/first/first.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func main() {
+
+	puzzleFile, _ := ioutil.ReadFile("../puzzle.txt")
+	puzzleContent := string(puzzleFile)
+	puzzles := strings.Split(puzzleContent, "\n\n")
+
+	allowedFields := []string{"byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"}
+	validPassports := 0
+	for _, passport := range puzzles {
+		countCheck := 0
+		for _, element := range allowedFields {
+			if strings.Contains(passport, element) {
+				countCheck++
+			}
+		}
+		if countCheck >= len(allowedFields) {
+			validPassports++
+		}
+	}
+
+	log.Printf("There are %d valid passports.", validPassports)
+}
